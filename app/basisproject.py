aa49615642c49efc9e877bb6180a9beaeafb8663
ThibaudWagemans <57669221+ThibaudWagemans@users.noreply.github.com>
2022-11-15 22:55:21 +0100

    add function

--- app/basisproject.py
+++ app/basisproject.py
@@ -74,9 +74,8 @@ async def get_GPU_by_power(power: int):
 #function to add a graphic card
 @app.post("/add/{name}/{price}/{memory}/{power}")
 async def add (name: str, price: int, memory: int, power: int):
-    return {name, price, memory, power}
-    #newGPU = GraphicCard(name, price, memory, power)
-    #GPUs.append(newGPU)
+    newGPU = GraphicCard(name, price, memory, power)
+    GPUs.append(newGPU)
     return GPUs
 
 #function to delete a graphic card
