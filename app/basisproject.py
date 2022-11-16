767c94c8d43b905be0248a88d7365f8013db63b3
ThibaudWagemans <57669221+ThibaudWagemans@users.noreply.github.com>
2022-11-16 22:17:45 +0100

    origins set to all for testing

--- app/basisproject.py
+++ app/basisproject.py
@@ -6,11 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:5500"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
