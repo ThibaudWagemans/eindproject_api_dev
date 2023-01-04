be8ba569e8822e06f8cc670aa5d269ff0306d951
ThibaudWagemans <57669221+ThibaudWagemans@users.noreply.github.com>
2023-01-04 21:18:17 +0100

    trouble launching api on okteto

--- app/models.py
+++ app/models.py
@@ -3,10 +3,32 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
-class User(Base):
+class GPUs(Base):
     __tablename__ = "GPUs"
     
-    name = Column(String, index=True)
+    name = Column(String, primary_key=True, index=True)
     price = Column(Integer, index=True)
     memory = Column(Integer, index=True)
     power = Column(Integer, index=True)
+
+    releaseDates = relationship("releaseDates", back_populates="GPUs")
+
+class releaseDates(Base):
+    __tablename__ = "releaseDates"
+    
+    name = Column(String, ForeignKey("GPUs.name"))
+    date = Column(String, primary_key=True, index=True)
+    GPU = relationship("GPUs", back_populates="releaseDates")
+    
+    GPUs = relationship("GPUs", back_populates="releaseDates")
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,49 @@ class GraphicCard (BaseModel) :
     price: int
     memory: int
     power: int
+
+class User (BaseModel) :
+    username: str
+    password: str
+
+class GraphicCard (GraphicCard) :
+    name: str
+    price: int
+    memory: int
+    power: int
+
+class User (User) :
+    username: str
+    password: str
+
+class updateGraphicCard (BaseModel) :
+    name: str
+    price: int
+    memory: int
+    power: int
+
+class deleteGraphicCard (BaseModel) :
+    name: str
+
+class addGraphicCard (BaseModel) :
+    name: str
+    price: int
+    memory: int
+    power: int
+
+class updateGraphicCard (updateGraphicCard) :
+    name: str
+    price: int
+    memory: int
+    power: int
+
+class deleteGraphicCard (deleteGraphicCard) :
+    name: str
+
+class addGraphicCard (addGraphicCard) :
+    name: str
+    price: int
+    memory: int
+    power: int
+
+
